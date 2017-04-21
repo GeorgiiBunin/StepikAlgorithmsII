331a90f8cdcb5be943e2060d820732aea925d9c6
George <diaq93@gmail.com>
2017-04-21 12:50:23 +0300

    initial commit

---
+++ src/Week_1/isBalanced.py
@@ -0,0 +1,57 @@
+class Stack(list):
+    """
+    a simple Stack implementation
+    """
+    def __init__(self):
+        self.data = []
+
+    def is_empty(self):
+        return not self.data
+
+    def push(self, val):
+        return self.data.append(val)
+
+    def top(self):
+        return self.data[-1]
+
+    def pop(self):
+        return self.data.pop()
+
+
+def isBalanced(s: str):
+    """
+    
+    :param s: string consists of any characters
+    :return: 'Success' if parentheses in s are balanced; 
+              else: index of a first right parenthesis without a pair if there is one;
+              otherwise: index of a first left parenthesis without a pair
+    
+    """
+    l_parentheses = ['(', '[', '{']
+    r_parentheses = [')', ']', '}']
+    stack = Stack()
+    i = 0
+    inds = [] #indices of left parentheses in s
+    for c in s:
+        i += 1
+        if c in l_parentheses:
+            stack.push(c)
+            inds += [i]
+        else:
+            if c in r_parentheses:
+                if stack.is_empty(): return i
+                top = stack.pop()
+                if ((c == ')' and top != '(') or
+                        (c == ']' and top != '[') or
+                        (c == '}' and top != '{')):
+                    return i
+                else:
+                    del inds[-1] # remove the index of the left parenthesis that just found a pair.
+    if stack.is_empty():
+        return 'Success'
+    else:
+        return inds[0]
+
+
+s = input()
+print(isBalanced(s))
